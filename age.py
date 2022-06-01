348acf592e7794ecc57bebfc197d6d4790a5c537
faglemans24 <faglemans24@greenhill.org>
2022-06-01 10:00:01 -0500

    second program

---
+++ age.py
@@ -0,0 +1,17 @@
+#Susie Fagelman
+#Calculate age
+#get use year and current year
+import os
+os.system ('cls')
+by=2006 #assign this a number
+#by =input ('year you were born') give us a string
+by = int( input ('year you were born,')) #type
+age=currentYear=2022 #also number
+age =currentYear-by
+print (your age is ',age')
+currentYear = 2022 #alsonumber
+age=currentYear-by
+print('your age is ',age)
+#Selection
+if age >50:
+    print('you are old')
