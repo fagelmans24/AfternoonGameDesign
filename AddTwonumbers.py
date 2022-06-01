9ce695fad8a6eb86cddcb876084a3b559d5b724d
faglemans24 <faglemans24@greenhill.org>
2022-06-01 09:37:55 -0500

    new one

---
+++ AddTwonumbers.py
@@ -0,0 +1,16 @@
+#Susie L Fagelman 
+#Two Numbers 
+# We will ask user for two numbers and add them
+# Anything after # is ignored by the computer
+#import things
+import os 
+os.system ('cls')
+#Use Variables 
+(Num1)=23
+(Num2)=12
+answer=(Num1+Num2)
+#Operators +,-,*,/
+print (Num1+Num2)
+print (Num1) #after each print it makes a return
+print (Num2, end= '')
+print (answer)
