a4f9dd7a98a31c56f2ad24f1d000ebdd522beb0b
faglemans24 <faglemans24@greenhill.org>
2022-06-02 17:38:45 -0500

    String Practice

---
+++ stringpractice.py
@@ -0,0 +1,27 @@
+#SusieFagelman
+#We are going to learn abt strings "" or ''
+import os
+os.system('cls')
+print('Hi')
+print ("Hi")
+print("Hi, let's go to the park")
+message = "You are awesome" #A is an array charcater
+# 0 1 2 3 4 5 #all arrays begin in zero
+# H E L L O 
+print(message)
+print(message[5]) #print the letter in position
+print(message[0:5]) #print all letter from position 0 to 4 5 characters
+if message. isdigit(): #isdigit is a method yoou must use it with a dot
+    sum=message +3 #if the statement is treu do this
+else: 
+    print(message+" I say so") #concentration    
+print(message. upper())
+print(message)
+if message.isupper():
+    print(message)
+else: 
+    print("i am in false") #use only for debugging I will delete or comment when i am done
+    message=message. upper () #making message permatitely upper case
+    print(message)
+print(type(message))
+print (dir(message)) #list of all methods
